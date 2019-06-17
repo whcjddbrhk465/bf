bd6467f2fd3746f882862bb40be15e462158b9ff
Wise Kim <wisekim@wisekimui-MacBook-Pro.local>
2019-06-17 19:28:23 +0900

    Updates

---
+++ precache-manifest.a881254182da0e893e33eeba3249ff3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4be98b96dc04d2a2ff9c4ebccb38a9",
+    "url": "/bf/index.html"
+  },
+  {
+    "revision": "a507d890b7c988f8b5d2",
+    "url": "/bf/static/css/main.425ef061.chunk.css"
+  },
+  {
+    "revision": "890392d0cae703c0d70c",
+    "url": "/bf/static/js/2.c76cc4b7.chunk.js"
+  },
+  {
+    "revision": "a507d890b7c988f8b5d2",
+    "url": "/bf/static/js/main.68e6844d.chunk.js"
+  },
+  {
+    "revision": "6113a8fd4c1b2a79d86b",
+    "url": "/bf/static/js/runtime~main.f63a7f35.js"
+  },
+  {
+    "revision": "0f330e2df3246bfbf3b69ec8580ce02d",
+    "url": "/bf/static/media/wallpaper.0f330e2d.jpg"
+  }
+]);
